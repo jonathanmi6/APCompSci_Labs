e86869f096e1dd449cf73fd1014cfe3985a07803
Jonathan Mi <34979278+jonathanmi6@users.noreply.github.com>
2018-08-30 10:41:17 -0700

    Added Lab_01

---
+++ Lab_01/Lab_01.java
@@ -0,0 +1,19 @@
+public class Test
+{
+	public static void main(String [] args)
+	{
+		for(int i = 0; i<2; i++)
+		{
+			System.out.println("     #  ####  #    #");
+			System.out.println("     # #    # ##   #");
+			System.out.println("     # #    # # #  #");
+			System.out.println("     # #    # #  # #");
+			System.out.println("#    # #    # #   ##");
+			System.out.println(" ####   ####  #    #");
+			
+			System.out.println("|\\---/|");
+			System.out.println("| o_o |");
+			System.out.println(" \\_^_/ ");
+		}
+	}
+}
