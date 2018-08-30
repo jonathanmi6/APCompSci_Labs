2012f83797f2fa14bed445d73169f6c7b0b3b648
Jonathan Mi <jonathan.mi6@gmail.com>
2018-08-30 11:37:06 -0700

    Added name file

---
+++ Lab_01/Lab_01_Name.java
@@ -0,0 +1,39 @@
+public class Lab_01_Name
+{
+	public static void main(String [] args)
+	{
+		
+		System.out.println("     #  ####  #    #");
+		System.out.println("     # #    # ##   #");
+		System.out.println("     # #    # # #  #");
+		System.out.println("     # #    # #  # #");
+		System.out.println("#    # #    # #   ##");
+		System.out.println(" ####   ####  #    #");
+		
+		
+		/*
+		System.out.println("|\\---/|");
+		System.out.println("| o_o |");
+		System.out.println(" \\_^_/ ");
+		*/
+		/*
+		System.out.println("     /\\_____/\\");
+		System.out.println("    /  o   o  \\");
+		System.out.println("   ( ==  ^  == )");
+		System.out.println("    )         (");
+		System.out.println("   (           )");
+		System.out.println("  ( (  )   (  ) )");
+		System.out.println(" (__(__)___(__)__)");
+		*/
+		/*   
+			     /\_____/\
+			    /  o   o  \
+			   ( ==  ^  == )
+			    )         (
+			   (           )
+			  ( (  )   (  ) )
+		     (__(__)___(__)__)
+		*/
+	
+	}
+}
